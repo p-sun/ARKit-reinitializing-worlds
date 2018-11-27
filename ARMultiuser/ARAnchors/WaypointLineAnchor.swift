80b65829a8071b00230f0e4e91237e983b144897
Paige Sun <paige.sun.dev@gmail.com>
2018-11-26 22:43:48 -0500

    Allow each tap to add a line from the previous tap for indoor wayfinding.

---
+++ ARMultiuser/ARAnchors/WaypointLineAnchor.swift
@@ -0,0 +1,47 @@
+//
+//  WaypointLineAnchor.swift
+//  ARMultiuser
+//
+//  Created by Paige Sun on 2018-11-26.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import ARKit
+
+class WaypointLineAnchor: ARAnchor {
+	
+	enum Keys: String {
+		case lengthKey = "lengthKey"
+	}
+	
+	var length: Float
+	
+	init(length: Float, transform: simd_float4x4) {
+		self.length = length
+		super.init(name: "Waypoint Anchor", transform: transform)
+	}
+	
+	required init(anchor: ARAnchor) {
+		length = (anchor as! WaypointLineAnchor).length
+		super.init(anchor: anchor)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {//
+//		guard let floatLength = aDecoder.decodeFloat(forKey: Keys.lengthKey.rawValue) else {
+//			return nil
+//		}
+		length = aDecoder.decodeFloat(forKey: Keys.lengthKey.rawValue)
+		
+		super.init(coder: aDecoder)
+	}
+	
+	override func encode(with aCoder: NSCoder) {
+		aCoder.encode(length, forKey: Keys.lengthKey.rawValue)
+		
+		super.encode(with: aCoder)
+	}
+	
+	override static var supportsSecureCoding: Bool {
+		return true
+	}
+}
